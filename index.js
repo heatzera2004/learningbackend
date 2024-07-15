fae998a38e3e4925085597792bcab903978ac836
heatzera2004 <yousuf04mar@gmail.com>
2024-07-15 14:11:40 +0530

    First Data push

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get('/' , (req,res) => {
     res.send("HELLO WORLD")
 })
 
+app.get('/datapush' , (req,res) => {
+    res.json({
+        message: "FIRST DATA PUSH"
+    })
+})
+
 app.listen(process.env.PORT , () => {
     console.log(`Server running on PORT : ${process.env.PORT}`)
 })
